ca57159b156d89846f0336d17ceda6280f294535
hadriensevel <hadrien.sevel@outlook.com>
2023-01-11 17:34:44 +0100

    Update

--- openritardi/api/data_objects.py
+++ openritardi/api/data_objects.py
@@ -5,7 +5,7 @@ class Station:
     '''Class to represent a station.
     '''
     
-    def __init__(self, name, id, name_short=None, lat=None, lon=None):
+    def __init__(self, name, id, name_short=None, lat=None, lon=None, id_region=None):
         '''
         :param name: name of the station
         :type name: str
@@ -17,14 +17,31 @@ class Station:
         :type lat: int, optional
         :param lon: longitude of the station, defaults to None
         :type lon: int, optional
+        :param id_region: ID of the region of the station, defaults to None
+        :type id_region: int, optional
         '''
-
         self.name = name
         self.id = id
         self.name_short = name_short
         self.lat = lat
         self.lon = lon
+        self.id_region = id_region
+
+    def __str__(self):
+        return (
+            f'Name: {self.name}\n'
+            f'Short name: {self.name_short}\n'
+            f'ID: {self.id}\n'
+            f'Latitude: {self.lat}\n'
+            f'Longitude: {self.lon}\n'
+            f'ID region: {self.id_region}'
+        )
+
 
 class Train:
     '''Class to represent a train.
+    '''
+
+class Journey:
+    '''Class to represent a journey.
     '''
--- openritardi/api/providers.py
+++ openritardi/api/providers.py
@@ -42,12 +42,14 @@ class Viaggiatreno:
                                     name_short=station['localita']['nomeBreve'],
                                     id=station['codiceStazione'],
                                     lat=station['lat'],
-                                    lon=station['lon']))
+                                    lon=station['lon'],
+                                    id_region=station['codReg']))
 
         return stations
 
     def autocomplete_station(self, query: str) -> list[Station]:
         '''Autocomplete a station name.
+        It returns a list of stations (name, short name and ID) that match the query.
 
         :param query: query to search
         :type query: str
@@ -69,4 +71,40 @@ class Viaggiatreno:
         return stations
 
     def get_region_station(self, id_station: str) -> int:
-        pass
+        '''Get the region ID of a station.
+
+        :param id_station: ID of the station
+        :type id_station: str
+        :return: ID of the region
+        :rtype: int
+        '''
+
+        # Get the data from the API
+        response = requests.get(self.BASE_URL + self.API_ENDPOINTS['region_station'] + '/' + id_station)
+        return int(response.text)
+        
+    def get_station_details(self, id_station: str) -> Station:
+        '''Create a Station object with its details.
+
+        :param id_station: _description_
+        :type id_station: str
+        :return: _description_
+        :rtype: Station
+        '''
+
+        # Get the ID of the region of the station
+        id_region = self.get_region_station(id_station)
+
+        # Get the data from the API
+        response = requests.get(self.BASE_URL + self.API_ENDPOINTS['station_details'] + '/' + id_station + '/' + str(id_region))
+        data_json = json.loads(response.text)
+
+        # Create a Station object
+        station = Station(  name=data_json['localita']['nomeLungo'],
+                            name_short=data_json['localita']['nomeBreve'],
+                            id=data_json['codiceStazione'],
+                            lat=data_json['lat'],
+                            lon=data_json['lon'],
+                            id_region=data_json['codReg'])
+
+        return station
