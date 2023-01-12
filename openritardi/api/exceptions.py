859bcafeaecb043470b79c2f3b913bd84deddd48
hadriensevel <hadrien.sevel@outlook.com>
2023-01-12 09:56:02 +0100

    Add custome exceptions

---
+++ openritardi/api/exceptions.py
@@ -0,0 +1,14 @@
+'''Custom exceptions for the API.
+'''
+
+
+class VoidResponse(Exception):
+    '''Exception raised when the API returns a void response.
+    '''
+    pass
+
+
+class ErrorResponse(Exception):
+    '''Exception raised when the API returns an error.
+    '''
+    pass
